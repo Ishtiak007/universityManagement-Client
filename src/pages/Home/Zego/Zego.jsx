9b2c348661d7cb4add786c7a6db871dde4d616cd
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-10-20 09:20:39 +0600

    Login button added in the Video Conference section

--- src/pages/Home/Zego/Zego.jsx
+++ src/pages/Home/Zego/Zego.jsx
@@ -13,7 +13,7 @@ const Zego = () => {
       <SectionTitle
         className="font-serif"
         heading={"Video Conference"}
-        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+      // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
       ></SectionTitle>
       <div class="flex">
         {/* <!-- Left Div (40%) --> */}
@@ -26,16 +26,21 @@ const Zego = () => {
             class="w-full p-2 border border-gray-300 rounded"
           />
           <button
-            class="mt-2 p-2 bg-blue-500 text-white rounded"
+            class="btn mt-2 bg-blue-500 text-white rounded"
             onClick={submitHandler}
           >
             Join
+          </button>
+          <button
+            class="btn mt-2 text-black rounded ml-5"
+          >
+            Login
           </button>
         </div>
 
         {/* <!-- Right Div (60%) --> */}
         <div class="w-3/5 bg-gray-100 p-4">
-        <p>This technology bridges geographical gaps, enabling real-time interactions that enhance the learning experience. It allows for virtual lectures, meetings, and group discussions, accommodating diverse learning styles and schedules. By facilitating remote access to educational resources and support services, video conferencing promotes inclusivity and engagement, ensuring that all members of the Rangpur Engineering College community can participate fully, regardless of their physical location. Furthermore, Rangpur Engineering College provides a flexible learning environment that supports a variety of educational needs, which is crucial in an era where online and hybrid education models are increasingly prevalent.</p>
+          <p>This technology bridges geographical gaps, enabling real-time interactions that enhance the learning experience. It allows for virtual lectures, meetings, and group discussions, accommodating diverse learning styles and schedules. By facilitating remote access to educational resources and support services, video conferencing promotes inclusivity and engagement, ensuring that all members of the Rangpur Engineering College community can participate fully, regardless of their physical location. Furthermore, Rangpur Engineering College provides a flexible learning environment that supports a variety of educational needs, which is crucial in an era where online and hybrid education models are increasingly prevalent.</p>
         </div>
       </div>
     </div>
