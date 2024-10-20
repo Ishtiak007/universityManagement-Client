1478525119f788e15c03c0de4868fdaed63fcf09
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-10-20 22:01:01 +0600

    Use admin created

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,20 @@
+import useAuth from "./useAuth";
+import useAxiosPublic from "./useAxiosPublic";
+
+
+
+const useAdmin = () => {
+    const { user, loading } = useAuth();
+    const axioxPublic = useAxiosPublic();
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axioxPublic.get(`/users/admin/${user.email}`)
+            return res.data?.admin;
+        }
+    });
+    return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
