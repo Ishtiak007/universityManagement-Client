32b5e1e9510ed4826b058f24e388795a53527e27
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-10-20 21:59:39 +0600

    Tanstack query setup done

--- src/main.jsx
+++ src/main.jsx
@@ -10,15 +10,26 @@ import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './pages/Providers/AuthProvider';
 import { Toaster } from 'react-hot-toast';
 
+
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Toaster />
-      <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster />
+        <HelmetProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
